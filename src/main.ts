89dc4ae2f2bf7bc45e7f2fdddc9dcf902ba7f8d8
danyil <r1sha@MacBook-Pro-Rivelij.local>
2025-05-25 17:13:57 +0300

    add: more drawing

--- src/main.ts
+++ src/main.ts
@@ -20,29 +20,50 @@ if (!ctx) {
 
 ctx.imageSmoothingEnabled = false;
 
+const Colors: Record<string, Color> = {
+    red: [255, 0, 0, 255],
+}
+
 const state = {
     mousedown: false,
+    activeColor: Colors.red,
+    brsushRadius: 8,
+    lastDrawnPixel: undefined as [number, number] | undefined,
 };
 
-canvas.addEventListener('mousedown', () => state.mousedown = true);
-canvas.addEventListener('mouseup', () => state.mousedown = false);
+canvas.addEventListener('mousedown', (ev: MouseEvent) => {
+    state.mousedown = true;
+});
+canvas.addEventListener('mouseup', () => {
+    state.mousedown = false;
+    state.lastDrawnPixel = undefined;
+});
 
 const [w, h] = [canvas.width, canvas.height];
 const image = ctx.createImageData(w, h);
 
-function update() {
+async function update() {
     ctx?.putImageData(image, 0, 0);
 }
 
-const red: Color = [255, 0, 0, 255];
-
-canvas.addEventListener('mousemove', (ev: MouseEvent) => {
+canvas.addEventListener('mousemove', async (ev: MouseEvent) => {
     if (state.mousedown === false) return;
 
-    console.log('drag', ev.x, ev.y);
-    setPixel(
-        ...screenCoordsToImageCoords(ev.clientX, ev.clientY), 
-        red,
+    if (!state.lastDrawnPixel) {
+        drawCircle(
+            ...screenCoordsToImageCoords(ev.clientX, ev.clientY), 
+            state.brsushRadius,
+        );
+    } else {
+        drawLine(
+            ...state.lastDrawnPixel,
+            ...screenCoordsToImageCoords(ev.clientX, ev.clientY), 
+            state.brsushRadius,
+        );
+    }
+
+    state.lastDrawnPixel = screenCoordsToImageCoords(
+        ev.clientX, ev.clientY,
     );
     update();
 });
@@ -72,5 +93,112 @@ function screenCoordsToImageCoords(x: number, y: number): [number, number] {
     ];
 }
 
+function drawLine(
+    fromX: number, fromY: number,
+    toX: number, toY: number,
+    radius: number,
+) {
+    const _deltaX = Math.abs(toX - fromX);
+    const _deltaY = Math.abs(toY - fromY);
+
+    // by default X is the main axis
+    // but if we're drawing a vertical line
+    // deltaX becomes close to zero so loop wile fromX < toX doesn't work anymore
+    // then main axis is one with the biggest (non-zero) delta
+    /** from MAIN axis */
+    let fromM = fromX;
+    /** from SECONDARY axis */
+    let fromS = fromY;
+    /** to MAIN axis */
+    let toM = toX;
+    /** to SECONDARY axis */
+    let toS = toY;
+    let mainAxisX = true;
+
+    // threshold should not be 1 for diagonal strokes to work properly
+    // otherwise it will frequently change between horizontal and vertical
+    // logic and thus draw crap
+    // 1.1 - 1.5 works good
+    if ((_deltaY / _deltaX) > AXIS_DETECT_THRESHOLD) {
+        fromM = fromY;
+        fromS = fromX;
+        toM = toY;
+        toS = toX;
+        mainAxisX = false;
+    }
+
+    if (toM < fromM) {
+        const [bufM, bufS] = [fromM, fromS];
+        fromM = toM; fromS = toS;
+        toM = bufM; toS = bufS;
+    }
+
+    const deltaM = toM - fromM;
+    const deltaS = toS - fromS;
+
+    const ds = deltaS / deltaM;
+    const dm = 1;
+
+    while (true) {
+        if (
+            Math.abs(fromM - toM) <= 1
+        ) {
+            break;
+        }
+
+        fromM += dm;
+        fromS += Math.floor(ds * dm);
+
+        drawCircle(
+            // if deltaX > deltaY then X is the main axis, else it is secondary 
+            mainAxisX ? fromM : fromS,
+            mainAxisX ? fromS : fromM,
+            radius,
+        );
+    }
+}
+
+function drawCircle(xc: number, yc: number, r: number) {
+    const circle = (xc: number, yc: number, x: number, y: number) => {
+        setPixel(xc+x, yc+y, state.activeColor);
+        setPixel(xc-x, yc+y, state.activeColor);
+        setPixel(xc+x, yc-y, state.activeColor);
+        setPixel(xc-x, yc-y, state.activeColor);
+        setPixel(xc+y, yc+x, state.activeColor);
+        setPixel(xc-y, yc+x, state.activeColor);
+        setPixel(xc+y, yc-x, state.activeColor);
+        setPixel(xc-y, yc-x, state.activeColor);
+    }
+
+    while (r > 0) {
+        let x = 0;
+        let y = r;
+        let d = 3 - 2 * r;
+
+        circle(xc, yc, x, y);
+
+        while (y >= x) {
+            if (d > 0) {
+                y--;
+                d = d + 4 * (x - y) + 10;
+            } else {
+                d = d + 4 * x + 6;
+            }
+
+            x++;
+            circle(xc, yc, x, y);
+        }
+
+        r--;
+    }
+}
+
 /** RGBA */
 type Color = [number, number, number, number];
+
+type Vector = {
+    x: number
+    y: number
+}
+
+const AXIS_DETECT_THRESHOLD = 1.1;
